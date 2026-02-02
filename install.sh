1b0fea708a1ae1189c03cb56f10266855a3c309e
Giorgi Dograshvili <76569084+DRAGOWN@users.noreply.github.com>
2026-02-02 21:15:43 +0400

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ..
-python -m venv draxter
-cd draxter
-./bin/pip install --upgrade pip
-./bin/python ./bin/pip install -r requirements.txt
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/C=GE/L=Tbilisi/O=DRAGOWN/OU=DRAGOWN/CN=localhost"
-./bin/python indb.py
