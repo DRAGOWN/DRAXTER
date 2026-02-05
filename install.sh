6c8e983fd1e5bf472861df4440be39d319cfd95f
Giorgi Dograshvili <76569084+DRAGOWN@users.noreply.github.com>
2026-02-05 12:43:23 +0400

    Install additional dependencies in install.sh
    Added installation of libssl1.1 and wkhtmltox packages.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ python -m venv DRAXTER
 cd DRAXTER
 ./bin/pip install --upgrade pip
 ./bin/python ./bin/pip install -r requirements.txt
+sudo dpkg -i tools/libssl1.1_1.1.1w-0+deb11u4_amd64.deb
 sudo dpkg -i tools/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/C=GE/L=Tbilisi/O=DRAGOWN/OU=DRAGOWN/CN=localhost"
 ./bin/python indb.py
